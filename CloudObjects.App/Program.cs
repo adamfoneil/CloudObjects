e52fcba884935e501a162ad60d8a76405483b5bf
Adam O'Neil <adamosoftware@gmail.com>
2020-10-18 18:15:35 -0400

    ignre connection.json

--- CloudObjects.App/Program.cs
+++ CloudObjects.App/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CloudObjects.App
 {
@@ -20,7 +15,8 @@ namespace CloudObjects.App
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(configure =>
                 {
-                    configure.AddJsonFile("Config/appsettings.json");                    
+                    configure.AddJsonFile("Config/appsettings.json");
+                    configure.AddJsonFile("Config/connection.json");
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
