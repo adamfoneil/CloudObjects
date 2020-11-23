0d59a1bdb7b8837064aeef80563e040451c91a8e
Vadim <vadim.a@outlook.com>
2020-11-23 22:03:40 +0300

    validators

---
+++ CloudObjects.App/Validators/AccountDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using CloudObjects.App.Interfaces;
+using CloudObjects.Models;
+using FluentValidation;
+
+namespace CloudObjects.App.Validators
+{
+    public class AccountDtoValidator : AbstractValidator<Account>, IDtoValidator
+    {
+        public AccountDtoValidator()
+        {
+            RuleFor(e => e.Key)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(e => e.Name)
+                .MinimumLength(5)
+                    .WithMessage("Account name must be least 5 characters long")
+                .MaximumLength(50)
+                    .WithMessage("Account name must be less than 50 characters long")
+                .Matches("^[a-zA-Z0-9\\.\\-]+$")
+                    .WithMessage("Account name contains one or more illegal characters, allowed only letters, digits, dots and dashes");
+        }
+    }
+}
---
+++ CloudObjects.App/Validators/StoredObjectDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using CloudObjects.App.Interfaces;
+using CloudObjects.Models;
+using FluentValidation;
+
+namespace CloudObjects.App.Validators
+{
+    public class StoredObjectDtoValidator : AbstractValidator<StoredObject>, IDtoValidator
+    {
+        private readonly Regex _notAllowedCharacters = new Regex("[?!@#$%^'|&*()+=;\"]+");
+
+        public StoredObjectDtoValidator()
+        {
+            RuleFor(e => e.Json)
+                .NotEmpty();
+
+            RuleFor(e => e.Name)
+                .MaximumLength(512)
+                .Must(name =>
+                {
+                    if (!Uri.IsWellFormedUriString(name, UriKind.Relative)) return false;
+                    if (_notAllowedCharacters.IsMatch(name)) return false;
+
+                    return true;
+                })
+                    .WithMessage($"Object Name must be well-formed URI string, and may not contain these characters: {string.Join(", ", _notAllowedCharacters)}.");
+        }
+    }
+}
